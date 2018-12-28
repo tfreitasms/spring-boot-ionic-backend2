be493a49bc1c467a011d1841e04d0f8149eed3ce
Thiago Freitas <tfreitas.ms@gmail.com>
2018-12-28 15:28:48 -0200

    Endpoint/pedidos/{id} disponibilizado

--- src/main/java/com/tfreitasms/cursomc/domain/PagamentoComBoleto.java
+++ src/main/java/com/tfreitasms/cursomc/domain/PagamentoComBoleto.java
@@ -4,13 +4,16 @@ import java.util.Date;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.tfreitasms.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
 public class PagamentoComBoleto extends Pagamento {
 	private static final long serialVersionUID = 1L;
 
+	@JsonFormat(pattern="dd/MM/yyyy")
 	private Date dataVencimento;
+	@JsonFormat(pattern="dd/MM/yyyy")
 	private Date dataPagamento;
 	
 	public PagamentoComBoleto() {
@@ -19,7 +22,7 @@ public class PagamentoComBoleto extends Pagamento {
 
 	public PagamentoComBoleto(Integer id, EstadoPagamento estado, Pedido pedido, Date dataVencimento, Date dataPagamento) {
 		super(id, estado, pedido);
-		this.dataPagamento = dataPagamento;
+				this.dataPagamento = dataPagamento;
 		this.dataVencimento = dataVencimento;
 	}
 
--- src/main/java/com/tfreitasms/cursomc/domain/Produto.java
+++ src/main/java/com/tfreitasms/cursomc/domain/Produto.java
@@ -15,7 +15,7 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Produto implements Serializable {
@@ -28,6 +28,7 @@ public class Produto implements Serializable {
 	private String nome;
 	private Double preco;
 	
+	@JsonIgnore
 	public List<Pedido> getPedidos() {
 		List<Pedido> lista = new ArrayList<>();
 		for (ItemPedido x : itens) {
@@ -42,9 +43,10 @@ public class Produto implements Serializable {
 		inverseJoinColumns = @JoinColumn(name = "categoria_id")
 			)
 	
-	@JsonBackReference
+	@JsonIgnore
 	private List<Categoria> categorias = new ArrayList<>();
 	
+	@JsonIgnore
 	@OneToMany(mappedBy="id.produto")
 	private Set<ItemPedido> itens = new HashSet<>();
 	
