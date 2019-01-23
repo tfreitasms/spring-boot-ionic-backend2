b8c11a25f003743ea5cbb67aea780bd35f62b4e6
Thiago Freitas <tfreitas.ms@gmail.com>
2019-01-23 17:45:17 -0200

    Salvando URL da imagem em Cliente

--- src/main/java/com/tfreitasms/cursomc/domain/Cliente.java
+++ src/main/java/com/tfreitasms/cursomc/domain/Cliente.java
@@ -56,6 +56,8 @@ public class Cliente implements Serializable {
 	@OneToMany(mappedBy="cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
+	private String imageUrl;
+	
 	public Cliente( ) {
 		addPerfil(Perfil.CLIENTE);
 		
@@ -176,6 +178,14 @@ public class Cliente implements Serializable {
 
 	public void setSenha(String senha) {
 		this.senha = senha;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
 	}
 	
 	
